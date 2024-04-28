12e3e81a608b759f79f53a96bcd0aa0fa4e99633
Paulalemany <palemany@ucm.es>
2024-04-28 13:02:31 +0200

    practica 2 implementada

---
+++ practica1-DSI/Assets/Scripts/Lab2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Lab2 : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        UIDocument document = GetComponent<UIDocument>();
+        VisualElement rootve = document.rootVisualElement;
+
+        List<VisualElement> lista_ve = rootve.Query(className: "elegido").ToList();
+
+        foreach (var item in lista_ve)
+        {
+            item.AddToClassList("yess");
+        }
+    }
+}
